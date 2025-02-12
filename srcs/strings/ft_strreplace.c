81bb3c13a274d464cd9e45fce46f9db75018f8a3
R0-main <justromixx@gmail.com>
2025-02-12 15:04:55 +0100

    replace safe_malloc by MALLOC

--- srcs/strings/ft_strreplace.c
+++ srcs/strings/ft_strreplace.c
@@ -6,7 +6,7 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 09:06:24 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/02/12 15:03:50 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/02/12 15:04:31 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strreplace(char *str, char *mask, char *value)
 	k = 0;
 	if (!str || !mask || !value)
 		return (NULL);
-	result = safe_malloc((ft_strlen(str) - ft_strlen(mask)) + ft_strlen(value)
+	result = MALLOC((ft_strlen(str) - ft_strlen(mask)) + ft_strlen(value)
 			+ 1);
 	while (str[i] && ft_strncmp(&str[i], mask, ft_strlen(mask)) != 0)
 		result[k++] = str[i++];
